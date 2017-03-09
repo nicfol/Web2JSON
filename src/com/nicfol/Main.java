2a23f41ad75af0aec0d03d9361c1095618f3251b
Nicolai <nfoldager0@gmail.com>
2017-03-10 00:52:54 +0100

    Can now classify educations based on the URL from ug.dk

--- src/com/nicfol/Main.java
+++ src/com/nicfol/Main.java
@@ -1,6 +1,5 @@
 package com.nicfol;
 
-import com.sun.xml.internal.fastinfoset.util.CharArray;
 import org.jsoup.Jsoup;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,7 +13,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String url = "https://www.ug.dk/uddannelser/universitetsuddannelser/bacheloruddannelser/humanistiskebacheloruddannelser/erhvervssprog/interkulturel-markedskommunikation";
+        String url = "https://www.ug.dk/uddannelser/universitetsuddannelser/bacheloruddannelser/naturvidenskabeligebacheloruddannelser/matematikfysikkemiogdatalogi/medialogy";
+
+        //Get edu type based on url and strip name cause faak u dashes
+        String[] eduType = findEdutype(url);
 
         String introduction = "";
         String description = "";
@@ -35,28 +37,30 @@ public class Main {
         introduction = cleanString(introduction);
 
         //Get description
-
         String row1 = "views-row views-row-1";
         try {
             description = doc.getElementsByClass(row1).first().getElementsByClass("field-content").toString();
         } catch (NullPointerException e) {
             System.out.println("No description class");
         }
-
         description = cleanString(description);
 
-        //Get title
-        String title = doc.title();
-        String titleNewString = title.replace(" | UddannelsesGuiden", "");
+        //Get title and strip  UG to get education name
+        String title = doc.title().replace(" | UddannelsesGuiden", "");
 
-        System.out.println(titleNewString);
+        System.out.println(title);
+        System.out.println();
+        for(int i = 0; i < eduType.length; i++)
+            System.out.println(eduType[i]);
         System.out.println();
         System.out.println(introduction);
         System.out.println();
         System.out.println(description);
     }
 
-    public static String cleanString(String str2Clean) {
+    //Cleans a string
+    private static String cleanString(String str2Clean) {
+        //Remove various html tags
         str2Clean = str2Clean.replace("<p>", "");
         str2Clean = str2Clean.replace("</p>", "");
         str2Clean = str2Clean.replace("<div class=\"field-content\">", "");
@@ -67,11 +71,29 @@ public class Main {
         str2Clean = str2Clean.replace("</strong>", "");
         str2Clean = str2Clean.replace("\n ", "\n");
 
+        //Clear leading and trailing new lines
         if(str2Clean.startsWith("\n"))
             str2Clean = str2Clean.substring(1, str2Clean.length());
         if(str2Clean.endsWith("\n"))
             str2Clean = str2Clean.substring(0, str2Clean.length()-1);
 
         return str2Clean;
+    }
+
+    private static String[] findEdutype(String url) {
+        //Strip protocol and domain
+        url = url.replace("https://", "");
+        url = url.replace("https://", "");
+        url = url.replace("www.ug.dk/uddannelser/", "");
+
+        //Split the string into an array based on the forward slashes
+        String eduArrTemp[] = url.split("/");
+
+        //Strip the array of the last position (name) cause it's useless and save to a new array
+
+        String[] eduType = new String[eduArrTemp.length-1];
+        System.arraycopy(eduArrTemp, 0, eduType, 0, eduArrTemp.length-1);
+
+        return eduType;
     }
 }

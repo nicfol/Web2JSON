3ff80992e0047c7acd6db862a35a7e102d62ef7d
Nicolai <nfoldager0@gmail.com>
2017-03-10 00:17:35 +0100

    cleans intro + description

--- src/com/nicfol/Main.java
+++ src/com/nicfol/Main.java
@@ -32,8 +32,10 @@ public class Main {
         } catch (NullPointerException e) {
             System.out.println("No intro class");
         }
+        introduction = cleanString(introduction);
 
         //Get description
+
         String row1 = "views-row views-row-1";
         try {
             description = doc.getElementsByClass(row1).first().getElementsByClass("field-content").toString();
@@ -41,27 +43,35 @@ public class Main {
             System.out.println("No description class");
         }
 
+        description = cleanString(description);
+
         //Get title
         String title = doc.title();
-        title.split("(?!^)");
-
-        char[] titleChar = title.toCharArray();
-        char[] titleNew = new char[title.length()];
-
-        for(int i = 0; i < title.length(); i++) {
-            if(titleChar[i] == '|' && titleChar[i+2] == 'U'){
-                break;
-            } else {
-                titleNew[i] = titleChar[i];
-            }
-        }
-        String titleNewString = new String(titleNew);
-
+        String titleNewString = title.replace(" | UddannelsesGuiden", "");
 
         System.out.println(titleNewString);
         System.out.println();
         System.out.println(introduction);
         System.out.println();
         System.out.println(description);
+    }
+
+    public static String cleanString(String str2Clean) {
+        str2Clean = str2Clean.replace("<p>", "");
+        str2Clean = str2Clean.replace("</p>", "");
+        str2Clean = str2Clean.replace("<div class=\"field-content\">", "");
+        str2Clean = str2Clean.replace("<div class=\"field-item even\">", "");
+        str2Clean = str2Clean.replace("</div>", "");
+        str2Clean = str2Clean.replace("&nbsp;", " ");
+        str2Clean = str2Clean.replace("<strong>", "");
+        str2Clean = str2Clean.replace("</strong>", "");
+        str2Clean = str2Clean.replace("\n ", "\n");
+
+        if(str2Clean.startsWith("\n"))
+            str2Clean = str2Clean.substring(1, str2Clean.length());
+        if(str2Clean.endsWith("\n"))
+            str2Clean = str2Clean.substring(0, str2Clean.length()-1);
+
+        return str2Clean;
     }
 }

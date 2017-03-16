7818f3f44076662b03b088f61f10076ee2756a43
Nicolai <nfoldager0@gmail.com>
2017-03-16 21:12:30 +0100

    Template for edu description

--- src/com/nicfol/DataCollector.java
+++ src/com/nicfol/DataCollector.java
@@ -1,22 +1,87 @@
 package com.nicfol;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class DataCollector {
     private final static String workingDirectory = System.getProperty("user.dir");           //Working Directory
 
-    public static void main(String[] args) {
-        ReadFile ugZoomReader = new ReadFile(workingDirectory + "\\input\\hovedtal2016.csv");
-        String ugZoomLine = "";
+    static Gson gson;
+    static EducationObj education;
+    static String gsonString;
 
-        while((ugZoomLine = ugZoomReader.readNextLine()) != null) {
-            System.out.println();
-            String ugSplitArr[] = ugZoomLine.split(";");
-            int cnt = 1;
-            for(int i = 0; i < ugSplitArr.length; i++) {
-                System.out.print(cnt + " | " + ugSplitArr[i]);
-                cnt++;
-            }
+    public static void main(String[] args) {
+        ReadFile ugZoomReader = new ReadFile(workingDirectory + "\\input\\ugZoomCleaned.csv");
+
+        //Debug first line
+        String ugZoomLine = ugZoomReader.readNextLine();
+        String firstLineArr[] = ugZoomLine.split(";");
+        for(int i = 0; i < firstLineArr.length-1; i++) {
+            //System.out.println("   | (" + i + ") " + firstLineArr[i]);
         }
 
+
+
+        Map<String,EducationObj> bachelorMap = new TreeMap<>();
+
+        while((ugZoomLine = ugZoomReader.readNextLine()) != null) {
+            String ugSplitArr[] = ugZoomLine.split(";");
+
+
+
+            String name = ugSplitArr[0];
+            String ugLink = ugSplitArr[1];
+            String educationLevel = ugSplitArr[3];
+            String institution = "Aalborg University";
+            String campus = ugSplitArr[4];
+            String[] educationForms = {"EduForm1", "eduForm2"};
+            float dropout_rate = 10.0f;
+            String completionTime = ugSplitArr[5];
+            //String[][] similarEducations;
+            //String[][] possibleJobs;
+
+            Map<Integer,Integer> applicants = new TreeMap<>();
+            Map<Integer,Integer> intake = new TreeMap<>();
+            Map<Integer,Integer> grades = new TreeMap<>();
+
+
+
+            applicants.put(2017, 1);
+            applicants.put(2016, 2);
+            applicants.put(2015, 3);
+
+            education = new EducationObj(name, ugLink, educationLevel, institution, campus, educationForms,
+                    dropout_rate, completionTime, applicants, intake, grades);
+
+            bachelorMap.put(name, education);
+
+            gson = new GsonBuilder().setPrettyPrinting().create();
+
+            gsonString = gson.toJson(education);
+        }
+
+        Map<String,EducationObj> masterMap = new TreeMap<>();
+
+
+
+
+        Map<String,Map> eduMap = new TreeMap<>();
+        eduMap.put("Bacheloruddannelse", bachelorMap);
+        eduMap.put("Kandidatuddannelse", masterMap);
+
+        String stringMap = gson.toJson(eduMap);
+
+        try (FileWriter file = new FileWriter(workingDirectory + "/output/eduDescTest.json")) {
+            file.write(stringMap);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/com/nicfol/EducationObj.java
+++ src/com/nicfol/EducationObj.java
@@ -1,31 +1,60 @@
 package com.nicfol;
 
+import sun.applet.resources.MsgAppletViewer;
+
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 public class EducationObj {
 
     String name;
+    String ugLink;
     String educationLevel;
     String institution;
     String campus;
-    int[][] applicants;
-    int[] intake;
-    float[] grades;
-    String[] educationForm;
+    String[] educationForms;
     float dropout_rate;
     String completionTime;
-    String ugLink;
     String[][] similarEducations;
     String[][] possibleJobs;
 
-    public EducationObj(String name, String educationLevel, int[][] applicants){
-        this.name = name;
-        this.educationLevel = educationLevel;
-        this.applicants = applicants;
-    }
+    Map<Integer,Integer> applicants;
+    Map<Integer,Integer> intake;
+    Map<Integer,Integer> grades;
 
-    public String getName() {
-        return this.name;
+
+    //int[][] applicants;
+    //int[] intake;
+    //float[] grades;
+
+    public EducationObj(
+                        String name,
+                        String ugLink,
+                        String educationLevel,
+                        String institution,
+                        String campus,
+                        String[] educationForms,
+                        float dropout_rate,
+                        String completionTime,
+                        //String[][] similarEducations,
+                        //String[][] possibleJobs,
+                        Map applicants,
+                        Map intake,
+                        Map grades) {
+
+        this.name = name;
+        this.ugLink = ugLink;
+        this.educationLevel = educationLevel;
+        this.institution = institution;
+        this.campus = campus;
+        this.educationForms = educationForms;
+        this.dropout_rate = dropout_rate;
+        this.completionTime = completionTime;
+        this.similarEducations = similarEducations;
+        this.possibleJobs = possibleJobs;
+
+        this.applicants = applicants;
+        this.intake = intake;
+        this.grades = grades;
     }
 }

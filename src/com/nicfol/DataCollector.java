218d940598553bb9ced86d4e3bf819fbb9027b88
Nicolai <nfoldager0@gmail.com>
2017-03-16 15:34:03 +0100

    You know, when do work on something and realise it's not even required?

--- src/com/nicfol/DataCollector.java
+++ src/com/nicfol/DataCollector.java
@@ -4,8 +4,18 @@ public class DataCollector {
     private final static String workingDirectory = System.getProperty("user.dir");           //Working Directory
 
     public static void main(String[] args) {
-        ReadFile ugZoomReader = new ReadFile(workingDirectory + "/input/ugZoom.csv");
+        ReadFile ugZoomReader = new ReadFile(workingDirectory + "\\input\\hovedtal2016.csv");
+        String ugZoomLine = "";
 
+        while((ugZoomLine = ugZoomReader.readNextLine()) != null) {
+            System.out.println();
+            String ugSplitArr[] = ugZoomLine.split(";");
+            int cnt = 1;
+            for(int i = 0; i < ugSplitArr.length; i++) {
+                System.out.print(cnt + " | " + ugSplitArr[i]);
+                cnt++;
+            }
+        }
 
 
     }
